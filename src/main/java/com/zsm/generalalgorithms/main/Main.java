474d115e896b700daae4e45dd4b8282a93eecd04
zengshengming <zengshengming@fsnip.com>
2018-02-25 22:06:53 +0800

    修改Main函数，清除无效代码

--- src/main/java/com/zsm/generalalgorithms/main/Main.java
+++ src/main/java/com/zsm/generalalgorithms/main/Main.java
@@ -1,8 +1,5 @@
 package com.zsm.generalalgorithms.main;
 
-import com.zsm.generalalgorithms.weixin.IDCardVerification;
-
-
 /**
  * @Author: zsm.
  * @Description:
@@ -13,10 +10,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        IDCardVerification idCardVerification = new IDCardVerification();
-        boolean b = idCardVerification.validateIDCard("511323199007086672");
-        //char d = idCardVerification.getValidateCode("511323199007086672");
-        sop(b);
+        
+    }
+
+    public static void sop(int[] arr)
+    {
+        System.out.println("");
+        for (int i : arr)
+        {
+            System.out.print(i + " ");
+        }
     }
 
     public static void sop(Object obj)
